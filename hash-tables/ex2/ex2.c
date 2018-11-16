905e24ba426000d811cf37a28a8419c7e7225a66
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-16 12:57:11 -0800

    ex2. passing test according to Brady's solution

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -10,9 +10,20 @@ char **reconstruct_trip(Ticket **tickets, int length)
   char **route = malloc(length * sizeof(char *));
 
   for (int i = 0; i < length; i++) {
-    hash_table_insert(hash, tickets[i]->source, tickets[i]->destination);
-    printf("%s     %s\n", tickets[i]->source, tickets[i]->destination);
+    if (strcmp(tickets[i]->source, "NONE") == 0) {
+      route[0] = tickets[i]->destination;
     }
+    hash_table_insert(hash, tickets[i]->source, tickets[i]->destination);
+  }
+
+  for (int j = 1; j<length; j++) {
+    if (hash_table_retrieve(hash, route[j-1]) != NULL) {
+      route[j] = hash_table_retrieve(hash, route[j-1]);
+    } else {
+      route[j] = "NONE";
+    }
+  }
+  destroy_hash_table(hash);
   return route;
 }
 

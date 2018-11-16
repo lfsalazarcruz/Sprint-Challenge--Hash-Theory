99057caa0c50d487624a6191609adacda91d6746
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-16 10:34:02 -0800

    ex.1 passing tests

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,9 +6,24 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *answer = malloc(sizeof(Answer));
 
-  // YOUR CODE HERE
+  for (int i = 0; i < length; i++) {
+    printf("looping %d \n", i);
+    int hash_retrieve = hash_table_retrieve(ht, weights[i]);
 
+    if(hash_retrieve != -1) {
+      answer->index_1 = i;
+      printf("Anwser->index_1: %d \n", answer->index_1);
+      answer->index_2 = hash_retrieve;
+      printf("Anwser->index_2: %d \n", answer->index_2);
+      destroy_hash_table(ht);
+      return answer;
+    }
+    else {
+      hash_table_insert(ht, limit-weights[i], i);
+    }
+  }
   return NULL;
 }
 
@@ -20,3 +35,19 @@ void print_answer(Answer *answer)
     printf("NULL\n");
   }
 }
+
+#ifndef TESTING
+int main(void)
+{
+  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+
+  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+
+  if (answer_4->index_1 != 6)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+  if (answer_4->index_2 != 2)
+    printf("Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+
+  return 0;
+}
+#endif
--- hash-tables/ex1/tests/ex1_tests.c
+++ hash-tables/ex1/tests/ex1_tests.c
@@ -6,22 +6,31 @@ char *test_ex1()
 {
   int weights_1 = {9};
   Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
+  printf("-1\n");
   mu_assert(answer_1 == NULL, "Your function did not return the expected answer for input {9}");
+  printf("passed +1\n\n");
 
   int weights_2[] = {4, 4};
   Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
+  printf("-2 \n");
   mu_assert(answer_2->index_1 == 1, "Your function did not return the expected answer for input {4, 4}");
   mu_assert(answer_2->index_2 == 0, "Your function did not return the expected answer for input {4, 4}");
+  printf("passed +2 \n\n");
 
   int weights_3[] = {4, 6, 10, 15, 16};
   Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
+  printf("-3 \n");
   mu_assert(answer_3->index_1 == 3, "Your function did not return the expected answer for input {4, 6, 10, 15, 16}");
   mu_assert(answer_3->index_2 == 1, "Your function did not return the expected answer for input {4, 6, 10, 15, 16}");
+  printf("passed +3 \n\n");
+
 
   int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
   Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  printf("-4 \n");
   mu_assert(answer_4->index_1 == 6, "Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
   mu_assert(answer_4->index_2 == 2, "Your function did not return the expected answer for input {12, 6, 7, 14, 19, 3, 0, 25, 40}");
+  printf("passed +4 \n\n");
 
   return NULL;
 }
